17c9a1afebcdc66668c67a0766b361ddfa16975c
Alan Huang <alan0988469026@gmail.com>
2020-11-09 23:31:39 +0800

    feat: fix test & add grid, special test

---
+++ test/generate_grid.py
@@ -0,0 +1,17 @@
+import math
+
+m = 1000
+n = 1000
+b = (m+n-2)*2
+input_file_name = f'{m}x{n}_grid.txt'
+
+with open(input_file_name, 'w') as f:
+    f.write(f'{m} {n} {b}')
+    f.write('\n')
+    f.write('R' + '0'*(n-1))
+    f.write('\n')
+    for i in range(math.floor((m-1)/2)):
+        f.write('0' + '10'*(math.floor((n-1)/2)))
+        f.write('\n')
+        f.write('0'*n)
+        f.write('\n')
