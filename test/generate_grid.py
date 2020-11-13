805d9d423111f865fe61a6930ae6247e44384a33
Alan Huang <alan0988469026@gmail.com>
2020-11-13 13:26:59 +0800

    feat: update more complex grid testcase

--- test/generate_grid.py
+++ test/generate_grid.py
@@ -1,18 +1,18 @@
 import math
 
 # **odd** number only for grid test case
-m = 5
-n = 5
+m = 99
+n = 99
 b = (m+n-2)*2
 input_file_name = f'{m}x{n}_grid'
 
 with open(input_file_name, 'w') as f:
     f.write(f'{m} {n} {b}')
     f.write('\n')
-    f.write('R' + '0'*(n-1))
+    f.write('R' + '01'*(math.floor((n-1)/2)))
     f.write('\n')
     for i in range(math.floor((m-1)/2)):
-        f.write('0' + '10'*(math.floor((n-1)/2)))
-        f.write('\n')
         f.write('0'*n)
+        f.write('\n')
+        f.write('1' + '01'*(math.floor((n-1)/2)))
         f.write('\n')
